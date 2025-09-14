d43db998a00c4f48ee3b7c6ee4d0e3ebcbdf1938
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-09-14 08:20:33 +0000

    Add files via upload

---
+++ get-dependencies.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+
+set -eux
+
+ARCH="$(uname -m)"
+EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+
+pacman -Syu --noconfirm \
+	base-devel       \
+	curl             \
+	git              \
+	gst-plugins-bad  \
+	gst-plugins-base \
+	gst-plugins-good \
+	libxcb           \
+	libxcursor       \
+	libxi            \
+	libxkbcommon     \
+	libxkbcommon-x11 \
+	libxrandr        \
+	libxtst          \
+	pipewire-audio   \
+	pulseaudio       \
+	pulseaudio-alsa  \
+	qt5ct            \
+	qt5-wayland      \
+	wget             \
+	xorg-server-xvfb \
+	zsync
+
+echo "Installing debloated packages..."
+echo "---------------------------------------------------------------"
+wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
+chmod +x ./get-debloated-pkgs.sh
+./get-debloated-pkgs.sh --add-opengl --prefer-nano libxml2-mini opus-mini
+
+echo "Building clementine..."
+echo "---------------------------------------------------------------"
+sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
+sed -i -e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
+git clone https://aur.archlinux.org/clementine.git ./clementine
+cd ./clementine
+makepkg -fs --noconfirm
+ls -la .
+pacman --noconfirm -U ./*.pkg.tar.*
+
+pacman -Q clementine | awk '{print $2; exit}' > ~/version
