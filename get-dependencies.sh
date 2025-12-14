8dd7cc9a540562b365f2bea73e5e6875ad778517
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-14 05:13:31 -0400

    refactor to template

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,48 +1,28 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
-ARCH="$(uname -m)"
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+ARCH=$(uname -m)
 
+echo "Installing package dependencies..."
+echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	base-devel       \
-	curl             \
-	git              \
 	gst-plugins-bad  \
 	gst-plugins-base \
 	gst-plugins-good \
-	libxcb           \
-	libxcursor       \
-	libxi            \
-	libxkbcommon     \
-	libxkbcommon-x11 \
-	libxrandr        \
-	libxtst          \
+	gst-libav        \
 	pipewire-audio   \
-	pulseaudio       \
-	pulseaudio-alsa  \
+	pipewire-jack    \
 	qt5ct            \
-	qt5-wayland      \
-	wget             \
-	xorg-server-xvfb \
-	zsync
+	qt5-wayland
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl --prefer-nano libxml2-mini opus-mini gdk-pixbuf2-mini librsvg-mini
+get-debloated-pkgs --add-opengl --prefer-nano ffmpeg-mini libxml2-mini icu-mini
 
-echo "Building clementine..."
-echo "---------------------------------------------------------------"
-sed -i -e 's|EUID == 0|EUID == 69|g' /usr/bin/makepkg
-sed -i -e 's|MAKEFLAGS=.*|MAKEFLAGS="-j$(nproc)"|; s|#MAKEFLAGS|MAKEFLAGS|' /etc/makepkg.conf
-git clone https://aur.archlinux.org/clementine.git ./clementine
-cd ./clementine
-sed -i -e "s|x86_64|$ARCH|" ./PKGBUILD
-makepkg -fs --noconfirm
-ls -la .
-pacman --noconfirm -U ./*.pkg.tar.*
+# Comment this out if you need an AUR package
+make-aur-package clementine
 
-pacman -Q clementine | awk '{print $2; exit}' > ~/version
+# If the application needs to be manually built that has to be done down here
