5528a8cfdb62fd85bfb4b1b2ce2d9cd3c9a41cc5
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-12-14 05:23:07 -0400

    no longer needed

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -18,8 +18,6 @@ pacman -Syu --noconfirm \
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
 get-debloated-pkgs --add-opengl --prefer-nano ffmpeg-mini libxml2-mini icu-mini
 
 # Comment this out if you need an AUR package
